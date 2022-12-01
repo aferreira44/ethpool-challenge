27f536ac880a8e7c4938b318dc237210cf76ce82
André Ferreira <contato@andreferreira.me>
2022-12-01 00:33:29 +0000

    Use hardhat-chai instead of waffle

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,8 +7,8 @@ import { task } from 'hardhat/config'
 // Plugins
 
 import '@nomiclabs/hardhat-ethers'
+import '@nomicfoundation/hardhat-chai-matchers'
 import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-waffle'
 import 'hardhat-abi-exporter'
 import 'hardhat-gas-reporter'
 import 'hardhat-contract-sizer'
